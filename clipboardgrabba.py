561734f207379125d1ea55488ecd5bd92b0077b4
Linus <linus.fraundorfer@enrag.at>
2021-04-07 16:56:16 +0200

    now saves image when I 'view image' (rightclick -> r)

--- clipboardgrabba.py
+++ clipboardgrabba.py
@@ -10,6 +10,39 @@ Created on Sun Apr  4 13:16:07 2021
 import urllib.request
 from tkinter import Tk
 from os import path
+from selenium import webdriver
+import os
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+def WaitForCopy():
+    browser = webdriver.Firefox()
+    url = "https://www.reddit.com/r/wow/"
+    url = "https://www.google.com/search?q=perro&client=firefox-b-d&hl=de&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiOwJzJpOzvAhUPCuwKHYJlB-QQ_AUoAnoECAEQBA&biw=1920&bih=1005"
+    browser.get(url)
+    location = r'C:\Users\Linus\Desktop'
+    filename = 'nbsrulez.jpg'
+    imageUrlCopied = False
+    fileExtensions = ['jpg', 'png', 'JPG', 'jpeg', 'webp', 'gif', 'svg', 'PNG', 'jfif', 'GIF', 'mp3']
+    while imageUrlCopied == False:
+        WebDriverWait(browser, 1000)
+        if any(extension in browser.current_url for extension in fileExtensions):
+            print(browser.current_url)
+            currl = browser.current_url
+            joinedurl = path.join(location,filename)
+            urllib.request.urlretrieve (currl, joinedurl)
+            browser.quit()
+            imageUrlCopied= True
+            print(' got it now down')
+
+        
+    # if not EC.url_contains('google.com'):
+        # # browser.quit()
+        # print('nr of windows reached.')
+
 
 def cbg(location, filename):
     myimage=Tk().clipboard_get()
@@ -17,5 +50,13 @@ def cbg(location, filename):
     urllib.request.urlretrieve (myimage, joinedurl)
     print('file successfully saved')
     
+    
+
+    
 lokation = r'C:\Users\Linus\Desktop'
-cbg(lokation, 'lphrules.jpg')
+# cbg(lokation, 'lphrules.jpg')
+WaitForCopy()
+
+
+
+#https://stackoverflow.com/questions/37906534/how-to-wait-for-a-specific-manual-action-using-selenium-python
