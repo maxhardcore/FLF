19fa8a8e28b6fd34f2f1507117333567d3c4079e
Linus <linus.fraundorfer@enrag.at>
2021-04-07 22:29:57 +0200

    will tell user to pick another image when encountering HTTP error. possibly increase sleep window? maybe 10 seconds will do?

--- clipboardgrabba.py
+++ clipboardgrabba.py
@@ -12,6 +12,8 @@ from tkinter import Tk
 from os import path
 from selenium import webdriver
 import os
+from urllib.error import HTTPError
+import time
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -25,24 +27,52 @@ def WaitForCopy(searchterm):
     url = f"https://www.google.com/search?site=&tbm=isch&source=hp&biw=1920&bih=1020&q={searchterm}"
     browser.get(url)
     location = r'C:\Users\Linus\Desktop'
-    filename = searchterm + '.jpg'
     imageUrlCopied = False
     fileExtensions = ['jpg', 'png', 'JPG', 'jpeg', 'webp', 'gif', 'svg', 'PNG', 'jfif', 'GIF', 'mp3']
     while imageUrlCopied == False:
         WebDriverWait(browser, 1000)
+        #as soon as url contains a link to an image
+
         if any(extension in browser.current_url for extension in fileExtensions):
             print(browser.current_url)
             currl = browser.current_url
-            joinedurl = path.join(location,filename)
-            urllib.request.urlretrieve (currl, joinedurl)
-            browser.quit()
-            imageUrlCopied= True
-            print(' got it now down')
+            
+            #check if filename exists already
+            i=1
+            filePath = CheckUniqueName(location, searchterm + '.jpg')
+            # joinedurl = path.join(location,searchterm + '.jpg')
+            try:
+                urllib.request.urlretrieve (currl, filePath)
+            except:
+                print('pick another image')
+                time.sleep(5)
+                continue                    
+            else:
+                browser.quit()
+                imageUrlCopied= True
+                print(' got it now down')
 
         
     # if not EC.url_contains('google.com'):
         # # browser.quit()
         # print('nr of windows reached.')
+
+def CheckUniqueName(location, searchterm):
+
+    base, extension = os.path.splitext(searchterm)
+    filePath = os.path.join(location, base + extension)
+    if os.path.exists(filePath):
+        i=1
+        while True:
+            newpath = "{0}{2}{1}".format(*path.splitext(filePath) + (i,))
+            if os.path.exists(newpath):
+                i+=1
+            else:
+                return(newpath)
+    return(filePath)
+
+
+            
 
 
 def cbg(location, filename):
@@ -56,7 +86,13 @@ def cbg(location, filename):
     
 lokation = r'C:\Users\Linus\Desktop'
 # cbg(lokation, 'lphrules.jpg')
+
 WaitForCopy('schlurl')
+    # except HTTPError:
+    #     p)
+    
+# rofl = CheckUniqueName(lokation, 'schlurl.jpg')
+# print(rofl, 'will be the new name')
 
 
 
